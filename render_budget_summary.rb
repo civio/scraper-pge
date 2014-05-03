f335f36fa58af1fd1738f4e59fb5cda3f91f8a08
David Cabo <david.cabo@gmail.com>
2014-05-03 21:23:29 +0200

    Refactor Mustache helper into own class

--- render_budget_summary.rb
+++ render_budget_summary.rb
@@ -6,52 +6,16 @@
 require 'csv'
 require 'mustache'
 
+require_relative 'lib/budget_summary_view'
+
 # XXX: This command line stuff is duplicated in the parser, should clean up
 budget_id = ARGV[0]
 year = budget_id[0..3]  # Sometimes there's a P for 'Proposed' at the end. Ignore that bit
 output_path = File.join(".", "output", budget_id)
 
-# Class containing the summary data
-class BudgetSummary < Mustache
-  def initialize(year)
-    @year = year
-    @summary = {
-      estado: {},
-      ooaa: {},
-      agencias: {},
-      otros: {},
-      seg_social: {}
-    }
-  end
-
-  def year; @year end
-
-  def estado_1_7; beautify sum(@summary[:estado], 7) end
-  def estado_1_9; beautify sum(@summary[:estado], 9) end
-  def estado_transfer; 0 end
-  def estado_total; beautify (sum(@summary[:estado], 9)-estado_transfer) end
-
-  def sum(breakdown, limit)
-    (1..limit).inject(0) {|sum, chapter| sum + breakdown[chapter.to_s] }
-  end
-
-  def beautify(i)
-    i.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")
-  end
-
-  def add_item(item)
-    is_income = (item.size == 7)
-    concept = item[is_income ? 2 : 3]
-    entity = item[1]
-    amount = item[is_income ? 6 : 7].to_i
-
-    return if concept.length > 1  # We need only top-level chapters
-    @summary[:estado][concept] = (@summary[:estado][concept]||0) + amount
-  end
-end
 
 # Do the calculations
-summary = BudgetSummary.new(year)
+summary = BudgetSummaryView.new(year)
 CSV.foreach(File.join(output_path, "ingresos.csv"), col_sep: ';') do |row|
   summary.add_item row
 end
