af59497409e955f4190e925d7e05bf63af5a3e2d
David Cabo <david.cabo@gmail.com>
2014-05-06 15:51:24 +0200

    Add checks of expenses against official sources

---
+++ lib/generic_breakdown.rb
@@ -0,0 +1,46 @@
+require 'nokogiri'
+require 'open-uri'
+
+# Basic parser for any sort of breakdowns that we don't need to parse fully, i.e. pages like [1].
+# It supports getting a particular cell by position or text description.
+#
+# [1]: http://www.sepg.pap.minhap.gob.es/Presup/PGE2014proyecto/MaestroDocumentos/PGE-ROM/doc/HTM/N_14_A_R_6_2_801_1_3.HTM
+#
+class GenericBreakdown < BaseBreakdown
+  def initialize(filename)
+    @filename = filename
+  end
+
+  # Retrieve a cell given its row and column position (-1 means last, as in Ruby)
+  def get_item_by_position(row_position, column_position)
+    # Get rows in HTML table, skipping header
+    rows = doc.css('table.S0ESTILO8 tr')[1..-1] # 2008 onwards (earlier?)
+
+    # Get the row we want
+    row = rows[row_position]
+
+    # Return the column we want
+    columns = row.css('td').map{|td| td.text.strip}
+    columns[column_position]
+  end
+
+  # Retrieve the right-most column for the row with the given description
+  def get_value_by_description(description, description_position=1)
+    # Get rows in HTML table, skipping header
+    rows = doc.css('table.S0ESTILO8 tr')[1..-1] # 2008 onwards (earlier?)
+    rows.map do |row|
+      columns = row.css('td').map{|td| td.text.strip}
+      next if columns[description_position]!=description
+
+      # We got to the right row, just return the rightmost column
+      return columns[-1]  
+    end
+  end
+
+  private
+
+  def doc
+    @doc = Nokogiri::HTML(open(@filename)) if @doc.nil?  # Lazy parsing of doc, only when needed
+    @doc
+  end
+end
